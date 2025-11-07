c58e18c24b60756ec0721001c55c78bdf46479dd
Eyan Chua <eyan@Eyans-MacBook-Air.local>
2025-11-08 02:29:49 +0800

    Remove build artifacts and update .gitignore

--- frontend/build/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gqmoY8q4aFZ4M4pusZVqYO9BBO0aRvYCQRrgtYCNLAI=\"\n}"
--- frontend/build/dev/static/chunks/frontend_src_app_page_tsx_f4785e63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_src_61bb726f._.js",
-  "static/chunks/9e883_4ffec74e._.js",
-  "static/chunks/frontend_src_App_41eb6576.css"
-],
-    source: "dynamic"
-});
