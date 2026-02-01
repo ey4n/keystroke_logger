322f1b3dff84bb807cec04472757df54485ea146
Param Srinivasan <134587139+curd45@users.noreply.github.com>
2026-02-01 11:21:28 +0800

    cookie storing update

--- frontend/src/app/todos/pages.tsx
+++ frontend/src/app/todos/pages.tsx
@@ -1,8 +1,8 @@
-import { createClient } from '../../utils/supabase/client'
+import { createClient } from '../../utils/supabase/server'
 import { cookies } from 'next/headers'
 
 export default async function Page() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
   const { data: todos } = await supabase.from('todos').select('*');
 
